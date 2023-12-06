6ad9e1823f12d816e7fa158f099acc86427c911b
mhernangilp <mhernangilp@gmail.com>
2023-12-06 10:38:59 +0100

    added colorinchis

--- minishell.h
+++ minishell.h
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
+# define INPUT_PRINT "\033[93mmsh > \033[0;0m"
+
 //execution.c
 void	execute(tline *line);
 
--- src/myshell.c
+++ src/myshell.c
@@ -5,13 +5,13 @@ int	main(void)
 	char	buf[1024];
 	tline	*line;
 
-	fputs("msh> ", stdout);
+	fputs(INPUT_PRINT, stdout);
 	while (fgets(buf, 1024, stdin)) {
 		line = tokenize(buf);
 		if (!line)
 			continue;
 		execute(line);
-		fputs("msh> ", stdout);
+		fputs(INPUT_PRINT, stdout);
 	}
 	return (0);
 }
