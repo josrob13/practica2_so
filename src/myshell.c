90eba97f0e8ba782a57c8338bbe9c43aa9e12f58
mhernangilp <mhernangilp@gmail.com>
2023-12-19 00:59:07 +0100

    removed duplicate functions and some comments

--- src/myshell.c
+++ src/myshell.c
@@ -12,17 +12,6 @@ int	main(void)
 	tline	*line;
 	t_list	*bg = NULL;
 
-	//#########################
-	/*int i = 0;
-	while (++i < 4) {
-		bgadd(&bg, i);
-	}
-	print_lst(&bg);
-	printf("-----------\nElimino\n-----------\n");
-	bgdelete(&bg, 2);
-	print_lst(&bg);*/
-	//#########################
-
 	signal(SIGINT, sig_handler_input);
 	fputs(INPUT_PRINT, stdout);
 	while (fgets(buf, 1024, stdin)) {
@@ -40,48 +29,4 @@ static void	sig_handler_input(int signal)
 {
 	if (g_sig == 1)
 		write(1, "\n\033[93mmsh > \033[0;0m", 19);
-}
-
-void	bgdelete(t_list **bg, int index)
-{
-	t_list	*aux, *aux2;
-
-	//se supone que antes se ha comprobado que el index no es mayor que el tamaño de la lista
-	aux = *bg;
-	if (index == 1) {
-		free(aux -> pids);
-		free(aux -> line);
-		*bg = aux -> next;
-		free(aux);
-	} else {
-		while (index > 1) {
-			aux2 = aux;
-			aux = aux -> next;
-			index--;
-		}
-		free(aux -> pids);
-		free(aux -> line);
-		aux2 -> next = aux -> next;
-		free(aux);
-	}
-}
-
-//Esta funcion solamente sirve para debuggear pero si se modifica sirve para hacer la funcion jobs
-void	print_lst(t_list **bg)
-{
-	t_list	*aux;
-	int	i;
-
-	aux = *bg;
-	while (aux) {
-		printf("--------\n");
-		printf("id: %d\n", aux -> id);
-		printf("pids: ");
-		i = -1;
-		while (aux -> pids[++i] != -1)
-			printf("%d ", aux -> pids[i]);
-		printf("\n");
-		printf("line: %s\n", aux -> line);
-		aux = aux -> next;
-	}
 }
