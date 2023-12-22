9eaa8bd765fa552a534bea224a604ab05068ae7e
mhernangilp <mhernangilp@gmail.com>
2023-12-22 14:51:48 +0100

    finished merging files

--- compila.sh
+++ compila.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gcc -c src/myshell.c -o myshell.o
-gcc myshell.o -o msh -I. -L parse/ -lparser -no-pie
+gcc -c myshell.c -o myshell.o
+gcc myshell.o -o msh -I. -L. -lparser -no-pie
 
 rm -rf *.o
 echo -e "\e[1;32mCompilation finished !\e[0m"
